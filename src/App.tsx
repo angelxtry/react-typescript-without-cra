54df888de17672275bf8efeca7b17b8b9442a3b5
angelxtry <angelxtry@gmail.com>
2020-09-18 22:15:52 +0900

    update
    ts-loader를 삭제하고 babel로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
 import React from 'react';
-import bulbIconUrl, { ReactComponent as BulbIcon } from './images/ic-btn-emo-bulb.svg';
-import loveIconUrl, { ReactComponent as LoveIcon } from './images/ic-btn-emo-love.svg';
-import moonIconUrl, { ReactComponent as MoonIcon } from './images/ic-btn-emo-moon.svg';
-import waterdropIconUrl, { ReactComponent as WaterdropIcon } from './images/ic-btn-emo-waterdrop.svg';
 
-export default function App() {
-  return (
-    <div>
-      React + TypeScript + Webpack!
-      <div>
-        <img src={bulbIconUrl} alt="Bulb Icon" />
-      </div>
-      <BulbIcon />
-      <div>
-        <img src={loveIconUrl} alt="Love Icon" />
-      </div>
-      <LoveIcon />
-      <div>
-        <img src={moonIconUrl} alt="Moon Icon" />
-      </div>
-      <MoonIcon />
-      <div>
-        <img src={waterdropIconUrl} alt="Waterdrop Icon" />
-      </div>
-      <WaterdropIcon />
-    </div>
-  );
+export default function App(): JSX.Element {
+  return <h1>React + TypeScript + Webpack + Without CRA</h1>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,57 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
-const mode = process.env.NODE_ENV || 'development';
+const DEVELOPMENT = 'development';
+const PRODUCTION = 'production';
 
-module.exports = {
-  mode,
+const mode = process.env.NODE_ENV || DEVELOPMENT;
+
+const config = {
+  context: path.resolve(__dirname, '.'),
+
   entry: {
-    app: path.join(__dirname, 'src', 'index.tsx'),
+    main: ['core-js', `./src/index.tsx`],
   },
+
   output: {
+    path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
   },
+
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: path.resolve('./dist'),
+    hot: true,
+    port: 9000,
+  },
+
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    modules: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    alias: {
+      react: require.resolve('react'),
+    },
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, './tsconfig.json'),
+      }),
+    ],
   },
+
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
+        test: /\.(ts|js)x?$/,
+        include: path.resolve('src'),
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.svg$/,
@@ -36,26 +65,34 @@ module.exports = {
           },
         ],
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader'],
-      // },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: './images/',
+        },
+      },
     ],
   },
+
   plugins: [
+    new webpack.SourceMapDevToolPlugin({}),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      templateParameters: {
-        env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
-      },
+      template: path.resolve(__dirname, 'public/index.html'),
       minify:
-        process.env.NODE_ENV === 'production'
+        mode === PRODUCTION
           ? {
               collapseWhitespace: true,
               removeComments: true,
             }
           : false,
     }),
-    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
 };
+
+module.exports = config;
